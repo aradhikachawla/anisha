b877775eb0a1a3fe8106ed74c02db360dec33a15
aradhika.chawla <aradhika.chawla@zscaler.com>
2022-12-26 10:14:02 -0800

    first commit

--- Main.java
+++ Main.java
@@ -12,6 +12,8 @@ public static class Main {
     new Movie("Pulp Fiction", "DVD", 8.8),
     new Movie("The Lord of the Rings: The Fellowship of the Ring", "DVD", 8.8)
         
-        System.out.println("Anisha changed");
+    System.out.println("Anisha changed");
+    System.out.println("Aradhika  changed");
+
 }
 
